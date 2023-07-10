7cbf66f424128f1e2935c7e896825ea0cb8f2e33
Garth Henson <garth@guahanweb.com>
2023-07-10 13:57:58 -0400

    adding utils and basic dice

--- py_munchkin/munchkin.py
+++ py_munchkin/munchkin.py
@@ -1,17 +1,25 @@
 import argparse
+from py_munchkin.utils.die import *
+
+def roll(args):
+    die = Die(args.sides)
+    value = die.roll()
+    print("Dice roll: %d" % (value))
 
 def main():
     parser = argparse.ArgumentParser(description="Munchkin")
     # Add your CLI commands, options, and arguments here
 
-    # Example command
-    parser.add_argument('filename', help='Name of the file')
+    # Create parser for the "roll" command
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    roll_parser = subparsers.add_parser('roll', help='roll help')
+    roll_parser.add_argument('--sides', type=int, default=6, help='--sides help')
+    roll_parser.set_defaults(func=roll)
 
     args = parser.parse_args()
+    args.func(args)
 
-    # Implement the logic for each command here
-    if args.filename:
-        print(f"Processing file: {args.filename}")
 
 if __name__ == '__main__':
     main()
---
+++ py_munchkin/utils/die.py
@@ -0,0 +1,30 @@
+import random
+
+class Die:
+    def __init__(self, sides = 6):
+        self.sides = sides
+
+    def roll(self):
+        return random.randint(1, self.sides)
+    
+class Dice:
+    def __init__(self, *argv):
+        list = []
+        for arg in argv:
+            sides = int(arg)
+            list.append(Die(sides))
+            self.dice = list
+                
+    def roll(self):
+        dice = []
+        total = 0
+
+        for die in self.dice:
+            value = die.roll()
+            dice.append(value)
+            total += value
+
+        d = dict()
+        d.dice = dice
+        d.total = total
+        return d
