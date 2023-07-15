34fed01b812697d2436a2cf63ade259fc9bb8195
Dsimpson21 <134972176+Dsimpson21@users.noreply.github.com>
2023-07-14 20:28:14 -0400

    Changed Test File to add "class"

---
+++ Test.py
@@ -0,0 +1,10 @@
+# Enemy Classes start
+
+class Villain:
+    def __int__(self, name, hp, defence, attack):
+        self.attributes = {
+            'name': name,
+            'hp': hp,
+            'defence': defence,
+            'attack': attack,
+        }
